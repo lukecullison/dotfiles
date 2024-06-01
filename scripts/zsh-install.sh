ce9859bb9159bd635fa6675bb954325e8b4defe3
Luke Cullison <lecullison17@gmail.com>
2024-05-31 23:33:08 -0400

    Zsh install script

---
+++ scripts/zsh-install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Exit script on any error
+set -e
+
+# Function to install dependencies on Ubuntu
+install_dependencies_ubuntu() {
+  sudo apt-get update
+  sudo apt-get install -y git zsh ripgrep tmux stow
+}
+
+# Function to install dependencies on Red Hat-based systems
+install_dependencies_redhat() {
+  sudo yum groupinstall -y 'Development Tools'
+  sudo yum install -y git zsh ripgrep tmux stow
+}
+
+# Detect the operating system
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  OS=$ID
+else
+  echo "Cannot detect the operating system."
+  exit 1
+fi
+
+# Install dependencies based on the operating system
+case $OS in
+  ubuntu)
+    install_dependencies_ubuntu
+    ;;
+  centos | rhel | fedora)
+    install_dependencies_redhat
+    ;;
+  *)
+    echo "Unsupported operating system: $OS"
+    exit 1
+    ;;
+esac
+
+# Clone the Neovim configuration repository
+if [ -f $HOME/.zshrc ]; then
+  mv -f $HOME/.zshrc $HOME/.zshrc.bak
+fi
+
+# Determine the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Construct the directory of the executable
+EXECUTABLE_DIR="${SCRIPT_DIR}/.."
+
+# Change to the executable's directory
+cd "$EXECUTABLE_DIR" || exit
+
+# Run stow command
+if [ -f /usr/bin/stow ]; then
+    /usr/bin/stow zsh
+else
+    echo "stow executable missing!"
+    exit 1
+fi
+
+echo "zsh configuration complete!"
