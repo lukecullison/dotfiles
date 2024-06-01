d61c8c71b3775f770e4db6a71ac3a27c43d553ce
Luke Cullison <lecullison17@gmail.com>
2024-05-31 23:37:28 -0400

    Added oh-my-zsh

--- scripts/zsh-install.sh
+++ scripts/zsh-install.sh
@@ -38,6 +38,10 @@ case $OS in
     ;;
 esac
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Clone the Neovim configuration repository
 if [ -f $HOME/.zshrc ]; then
   mv -f $HOME/.zshrc $HOME/.zshrc.bak
