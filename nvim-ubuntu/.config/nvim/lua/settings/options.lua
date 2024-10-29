6cab9613c52ae53ac9905acff79db6e7ad1d9e83
Luke Cullison <lecullison17@gmail.com>
2024-10-28 21:05:37 -0400

    added ubuntu configs

---
+++ nvim-ubuntu/.config/nvim/lua/settings/options.lua
@@ -0,0 +1,12 @@
+-- Tab settings
+vim.o.expandtab = true   -- Use spaces instead of tabs
+vim.o.shiftwidth = 4     -- Size of an indent
+vim.o.tabstop = 4        -- Number of spaces tabs count for
+vim.o.softtabstop = 4    -- Number of spaces tabs count for while editing
+
+-- Search settings
+vim.o.ignorecase = true  -- Case insensitive searching
+vim.o.smartcase = true   -- Case sensitive if search contains uppercase
+
+-- Enable line numbers
+vim.opt.number = true
